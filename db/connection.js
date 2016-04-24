7ac725610461654d0d5315b1890e3654bb1f2d61
Clarissa Stark <clarissa.stark@gmail.com>
2016-04-24 19:42:53 -0400

    connected to mongoose

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,27 @@
-var seedData = require("./seeds.json");
+var seedData = require("./senators_seeds.json");
+var mongoose = require("mongoose");
+
+var UserReviewSchema = new mongoose.Schema (
+  {
+    author: String,
+    senator: String,
+    body: String,
+    date: Date
+  }
+);
+
+var SenatorSchema = new mongoose.Schema (
+  {
+    firstName: String,
+    lastName: String,
+    party: String,
+    state: String
+  }
+);
+
+mongoose.model("Senator", SenatorSchema);
+mongoose.model("UserReview", UserReviewSchema);
+mongoose.connect("mongodb://localhost/senatescore")
 
 module.exports = {
   senators: seedData
