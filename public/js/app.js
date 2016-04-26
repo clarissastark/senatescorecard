aa77b214a21cebcf21ecac8eed10e7204188124d
Clarissa Stark <clarissa.stark@gmail.com>
2016-04-26 17:32:02 -0400

    added angular dependencies

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,14 @@
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '1724449767833013',
-    xfbml      : true,
-    version    : 'v2.6'
-  });
-};
+"use strict";
 
-(function(d, s, id){
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+(function(){
+  angular
+  .module("senatescore", [
+    "ui-router",
+    "ngResource"
+  ]);
+
+})();
+
+
 
 console.log("JavaScript's working!");
