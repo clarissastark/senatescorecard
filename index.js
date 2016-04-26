1134e9db048b5cabf7d99fabfff33c38a51d10d2
Clarissa Stark <clarissa.stark@gmail.com>
2016-04-26 09:48:17 -0400

    completes twitter user authentication and fetches user data from API

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ process.env.t_consumer_key = env.t_consumer_key;
 process.env.t_consumer_secret = env.t_consumer_secret;
 
 app.use(session({
-  secret: "random string",
+  secret: process.env.session_secret,
   resave: false,
   saveUninitialized: false,
   store: new SMongo({
@@ -30,7 +30,6 @@ app.use(session({
 
 app.set("port", process.env.PORT || 3001);
 app.set("view engine", "hbs");
-
 app.engine(".hbs", hbs({
   extname:      ".hbs",
   partialsDir:  "views/",
@@ -38,9 +37,12 @@ app.engine(".hbs", hbs({
   defaultLayout: "layout-main"
 })
 );
-
 app.use("/assets", express.static("public"));
 app.use(parser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.render("app-welcome");
+});
 
 app.get("/login/twitter", function(req, res){
   var url = "https://api.twitter.com/oauth/request_token";
@@ -48,18 +50,48 @@ app.get("/login/twitter", function(req, res){
     callback:         process.env.t_callback_url,
     consumer_key:     process.env.t_consumer_key,
     consumer_secret:  process.env.t_consumer_secret
-  };
+  }
   request.post({url: url, oauth: oauth}, function(e, response){
     var auth_data = qstring.parse(response.body);
     var post_data = qstring.stringify({oauth_token: auth_data.oauth_token});
-    req.session.t_oauth_token = auth_data.oauth_token;
-    req.session.t_oauth_token_secret = auth_data.oauth_token_secret;
+    req.session.t_oauth_token         = auth_data.oauth_token;
+    req.session.t_oauth_token_secret  = auth_data.oauth_token_secret;
     res.redirect("https://api.twitter.com/oauth/authenticate?" + post_data);
   });
 });
 
-app.get("/", function(req, res){
-  res.render("welcome-page");
+app.get("/login/twitter/callback", function(req, res){
+  var url = "https://api.twitter.com/oauth/access_token";
+  var auth_data = qstring.parse(req.query);
+  var oauth = {
+    consumer_key:     process.env.t_consumer_key,
+    consumer_secret:  process.env.t_consumer_secret,
+    token:            req.session.t_oauth_token,
+    token_secret:     req.session.t_oauth_token_secret,
+    verifier:         auth_data.oauth_verifier
+  };
+  // third leg of oauth authentication
+  request.post({url: url, oauth: oauth}, function(e, response){
+    var auth_data = qstring.parse(response.body);
+    req.session.t_user_id = auth_data.user_id;
+    req.session.t_screen_name = auth_data.screen_name;
+    req.session.t_oauth = {
+      consumer_key:     process.env.t_consumer_key,
+      consumer_secret:  process.env.t_consumer_secret,
+      token:            auth_data.oauth_token,
+      token_secret:     auth_data.oauth_token_secret
+    };
+    request.get({
+      url:           "https://api.twitter.com/1.1/useres/show.json",
+      json:          true,
+      oauth:         req.session.t_oauth,
+      qs:            {
+        screen_name: req.session.t_screen_name
+      }
+    }, function(e, response){
+      res.json(response.body);
+    });
+  });
 });
 
 app.get("/senators", function(req,res){
