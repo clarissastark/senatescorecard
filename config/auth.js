fafc2152a5f593bb51a2d81500382c74c10dc71e
Clarissa Stark <clarissa.stark@gmail.com>
2016-04-26 17:14:42 -0400

    updated callback urls for facebook and twitter in auth.js for heroku

--- config/auth.js
+++ config/auth.js
@@ -1,21 +1,23 @@
 module.exports = {
 
-    'facebookAuth' : {
-        'clientID'      : '1724449767833013',
-        'clientSecret'  : 'ddf49de85bd044232463b364b8270d4a',
-        'callbackURL'   : 'http://localhost:3001/auth/facebook/callback'
+    "facebookAuth" : {
+        "clientID"      : "1724449767833013",
+        "clientSecret"  : "ddf49de85bd044232463b364b8270d4a",
+        // "callbackURL"   : "http://localhost:3001/auth/facebook/callback"
+        "callbackURL"   : "https://senatescore.herokuapp.com/auth/facebook/callback"
     },
 
-    'twitterAuth' : {
-        'consumerKey'       : 'AwoDgM3gjLpD1VCF8kQcb5ydT',
-        'consumerSecret'    : 'Ro4qOygDORSdzdg5ZOxkLRe0eX3sGh2U8q5BfqA4X53VYV61Yk',
-        'callbackURL'       : 'http://127.0.0.1:3001/auth/twitter/callback'
+    "twitterAuth" : {
+        "consumerKey"       : "AwoDgM3gjLpD1VCF8kQcb5ydT",
+        "consumerSecret"    : "Ro4qOygDORSdzdg5ZOxkLRe0eX3sGh2U8q5BfqA4X53VYV61Yk",
+        // "callbackURL"       : "http://127.0.0.1:3001/auth/twitter/callback"
+        "callbackURL"       : "https://senatescore.herokuapp.com/auth/twitter/callback"
     },
 
-    'googleAuth' : {
-        'clientID'      : 'your-secret-clientID-here',
-        'clientSecret'  : 'your-client-secret-here',
-        'callbackURL'   : 'http://localhost:3001/auth/google/callback'
+    "googleAuth" : {
+        "clientID"      : "your-secret-clientID-here",
+        "clientSecret"  : "your-client-secret-here",
+        "callbackURL"   : "http://localhost:3001/auth/google/callback"
     }
 
 };
