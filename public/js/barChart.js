30587f5814386117c129629748b7cfe5c011c890
Clarissa Stark <clarissa.stark@gmail.com>
2016-04-29 13:31:31 -0400

    added hover to senate index view and fixed button removal error

--- public/js/barChart.js
+++ public/js/barChart.js
@@ -1,14 +1,64 @@
-// barChart.draw() = function(){
-// var data = showVM.senators.score2015;
-//   d3.select(".chart")
-// 	.selectAll("div")
-// 	.data(data)
-// 	.enter()
-// 	.append("div")
-// 	.style("width", function(d){
-// 	  return d * 10 + "px"
-// 	})
-// 	.text(function(d){
-// 	  return d;
-// 	});
-// }
+var drawBarChart = function(){
+
+  var results = [
+    {party:'Conservative', seats:331 },
+    {party:'Labour', seats:232},
+    {party:'SNP', seats:56},
+    {party:'Liberal Democats', seats:8},
+    {party:'DUP', seats:8},
+    {party:'Other', seats:15}
+  ];
+
+  var gridWidth = 80;
+  var gridHeight = 5;
+  var layout = d3_iconarray.layout()
+  .width(gridWidth)
+  .height(gridHeight);
+  var width = 600;
+  var height = 40;
+  var radius = 2.5;
+  var margin = { top:radius*2, left:radius*2, bottom:radius*2, right:radius*2 }
+  var scale = d3.scaleLinear()
+  .range([0, (width-(margin.left + margin.right))])
+  .domain([0, gridWidth]);
+
+
+  console.log(scale.range())
+
+  d3.select('#width-first')
+  .selectAll('div.result')
+  .data(results)
+  .enter()
+  .append('div').attr('class','result')
+  .call(arrayBars, true);
+
+  d3.select('#height-first')
+  .selectAll('div.result')
+  .data(results)
+  .enter()
+  .append('div').attr('class','result')
+  .call(arrayBars, false);
+
+  function arrayBars(parent, widthFirst){
+    layout.widthFirst(widthFirst);
+
+    parent.append('p')
+    .attr('class','bar-label')
+    .html(function(d){
+      return d.party;
+    });
+
+    parent.append('svg')
+    .attr('width', width).attr('height', height)
+    .append('g')
+    .attr('transform','translate('+margin.left+','+margin.top+')')
+    .attr('class',function(d){return d.party})
+    .selectAll('circle')
+    .data(function(d){ return layout( d3.range(0, d.seats, 1) ); })
+    .enter()
+    .append('circle')
+    .attr('cx',function(d){ return scale(d.position.x); })
+    .attr('cy',function(d){ return scale(d.position.y); })
+    .attr('r', radius)
+  }
+}
