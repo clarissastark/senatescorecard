6e4fe1419160b612a626afc6df9cccba30a34ccb
Clarissa Stark <clarissa.stark@gmail.com>
2016-05-04 15:19:56 -0400

    reverted seed.js file to status prior to attempts to re-seed heroku

--- db/seed.js
+++ db/seed.js
@@ -1,13 +1,13 @@
-var mongodb = require('mongodb');
+// var mongodb = require('mongodb');
 var mongoose = require("./connection");
 var seedData = require("./senators_seeds.json");
-var uri = "mongodb://heroku_phhfprc2:q8om49jsukmpjkeg5f8ekp68u0@ds019481.mlab.com:19481/heroku_phhfprc2";
+// var uri = "mongodb://heroku_phhfprc2:q8om49jsukmpjkeg5f8ekp68u0@ds019481.mlab.com:19481/heroku_phhfprc2";
 
 var Senator = mongoose.model("Senator");
 
-mongodb.MongoClient.connect(uri, { server: { auto_reconnect: true } }, function (err, db) {
-    console.log(err);
-});
+// mongodb.MongoClient.connect(uri, { server: { auto_reconnect: true } }, function (err, db) {
+//     console.log(err);
+// });
 
 Senator.remove({}).then(function(){
   Senator.collection.insert(seedData).then(function(){
